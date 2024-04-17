426f904dd386a95511264c862fab29cfe6389d78
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2024-04-17 21:26:42 +0700

    JWT authenticate

--- Tour-De-Viet/server/FlightService.js
+++ Tour-De-Viet/server/FlightService.js
@@ -18,48 +18,73 @@ class Flight{
     }
     
     async createFlights(newData) {
-        return fetch(`${baseUrl}/api/flights`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "flightID":newData.flightID,
-                "flightName":newData.flightName, 
-                "startDate":newData.startDate, 
-                "endDate" :newData.endDate, 
-                "price" :newData.price, 
-                "goFrom" :newData.goFrom, 
-                "arriveAt":newData.arriveAt,
-            })
-        })
-            .then(response => response.json())
-            .catch(error => {
-                console.error('Error creating flights:', error);
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+            throw new Error('Token not found');
+            }
+            const response = await fetch(`${baseUrl}/api/flights`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "flightID": newData.flightID,
+                    "flightName": newData.flightName, 
+                    "startDate": newData.startDate, 
+                    "endDate": newData.endDate, 
+                    "price": newData.price, 
+                    "goFrom": newData.goFrom, 
+                    "arriveAt": newData.arriveAt,
+                })
             });
+            if (!response.ok) {
+                throw new Error('Failed to create flights');
+            }
+            return response.json();
+        } catch (error) {
+            console.error('Error creating flights:', error);
+            if (error.message === 'Token not found'){
+                window.location.href('/login');
+              }
+            throw error;
+        }
     }
     
-    async updateFlights(newData) {
-        return fetch(`${baseUrl}/api/flights/${flightID}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "flightID":newData.flightID,
-                "flightName":newData.flightName, 
-                "startDate":newData.startDate, 
-                "endDate" :newData.endDate, 
-                "price" :newData.price, 
-                "goFrom" :newData.goFrom, 
-                "arriveAt":newData.arriveAt,
-            })
-        })
-        .then(response => response.json())
-        .catch(error => {
-            console.error('Error updating flights', error);
-        });
+    async updateFlights(flightID, newData) {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+            throw new Error('Token not found');
+            }
+            const response = await fetch(`${baseUrl}/api/flights/${flightID}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "flightID": newData.flightID,
+                    "flightName": newData.flightName, 
+                    "startDate": newData.startDate, 
+                    "endDate": newData.endDate, 
+                    "price": newData.price, 
+                    "goFrom": newData.goFrom, 
+                    "arriveAt": newData.arriveAt,
+                })
+            });
+            if (!response.ok) {
+                throw new Error('Failed to update flights');
+            }
+            return response.json();
+        } catch (error) {
+            console.error('Error updating flights:', error);
+            if (error.message === 'Token not found'){
+                window.location.href('/login');
+              }
+            throw error;
+        }
     }
+    
 }
