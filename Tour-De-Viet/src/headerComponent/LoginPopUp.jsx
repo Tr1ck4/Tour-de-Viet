8923040fa3981bc63cfd3ed28548ee82f57d6723
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-04-26 14:51:21 +0700

    login popup ui

--- Tour-De-Viet/src/headerComponent/LoginPopUp.jsx
+++ Tour-De-Viet/src/headerComponent/LoginPopUp.jsx
@@ -13,11 +13,15 @@ const LoginPopUp = () => {
     return (
         <div className=' fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center '>
             <div className='w-[400px] h-[300px] backdrop-blur-lg drop-shadow-xl bg-bone-white bg-opacity-10 shadow-lg stroke-rgba(255, 255, 255, 1) rounded-2xl'>
-                <div className='my-10'>
-                    <form onSubmit={handleSubmit}>
-                        <input className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl formInput' type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                        <input className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl my-5 formInput' type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <button className='w-2/5 bg-bone-white h-[60px] mx-auto rounded-2xl' type="submit" onClick={handleSubmit}>Login</button>
+                <div className='my-8'>
+                    <form className='' onSubmit={handleSubmit}>
+                        <div className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl'>
+                            <input className='formInput' type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        </div>
+                        <div className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl my-6'>
+                            <input className='formInput' type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <button className='w-2/5 bg-bone-white h-[60px] mx-auto rounded-2xl font-itim button' type="submit" onClick={handleSubmit}>Login</button>
                     </form>
                     {/* <div className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl'></div>
                     <div className='w-3/4 bg-bone-white h-[60px] mx-auto rounded-2xl my-5'></div>
