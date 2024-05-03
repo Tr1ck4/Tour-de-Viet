a50f6534e9e09e869b21d9bcfc04f52974bf50b0
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2024-05-03 10:59:19 +0700

    Revert "Merge branch 'main' into Tour-page"
    This reverts commit 441254f578c7ddd266d65df3da41877ae4da12cc, reversing
    changes made to b9ffad9dd8a17b2a6121331345491a4ec536bc41.

--- Tour-De-Viet/src/main.jsx
+++ Tour-De-Viet/src/main.jsx
@@ -1,8 +1,7 @@
 import './index.css'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { gsap } from 'gsap';
 
 import Header from './Header.jsx'
 import ParallaxPage from './Parallax.jsx'
@@ -13,7 +12,6 @@ import TourDetailPage from './TourDetailPage.jsx';
 import TestUI from './testUI.jsx';
 import BookTourPage from './BookTourPage.jsx';
 import ProfilePage from './Profile.jsx';
-import Bot from './Bot.jsx';
 
 
 export default function App() {
@@ -30,19 +28,16 @@ export default function App() {
         <Route path='/testUI' element={<TestUI />} />
         <Route path='/bookinghistory' element={<BookTourPage />} />
         <Route path='/profile' element={<ProfilePage />} />
-        <Route path = '/bot' element={<Bot/>} />
       </Routes>
     </Router>
   );
 }
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
-const parallax_el = document.querySelectorAll(".parallax");
 
 document.addEventListener('mousemove', parallax);
 
@@ -51,14 +46,9 @@ function parallax(e) {
 
     const x = e.pageX - window.innerWidth / 2;
     const y = e.pageY - window.innerHeight / 2;
-    
-let x = 0, y = 0,rotateDeg = 0;
-function update(cursorPosition) {
-  parallax_el.forEach((layer) => {
-  
+
     let speedx = layer.dataset.speedx;
     let speedy = layer.dataset.speedy;
-    let speedz = layer.dataset.speedz;
 
     let isInLeft = parseFloat(getComputedStyle(layer).left) < window.innerWidth / 2 ? 1 : -1;
     let zValue = e.pageX - parseFloat(getComputedStyle(layer).left) * isInLeft * 0.1;
@@ -70,23 +60,6 @@ function update(cursorPosition) {
 
     layer.style.transform = `translateX(calc(-50% + ${-x * speedx}px)) translateY(calc(-50% + ${y * speedy}px)) 
     perspective(2300px) translateZ(${zValue * speedz}px) rotateY(${rotateDeg * rote}deg)`;
-    let isInLeft = parseFloat(getComputedStyle(layer).left) < window.innerWidth/2 ? 1:-1;
-    let zValue = cursorPosition - parseFloat(getComputedStyle(layer).left) * isInLeft * 0.1;
-    let rote = layer.dataset.rotation;
-
-    layer.style.transform = `
-    perspective(2300px) translateX(calc(-50% + ${-x * speedx}px)) translateY(calc(-50% + ${y * speedy}px)) 
-    translateZ(${zValue * speedz}px) rotateY(${rotateDeg * rote}deg)`;
   });
 }
-
-update(0);
-window.addEventListener("mousemove",(e)=>{
-  x = e.clientX - window.innerWidth/2;
-  y = e.clientY - window.innerHeight/2;
-
-  rotateDeg = (x / (window.innerWidth/2))*10;
-  update(e.clientX);
-});
-
 
